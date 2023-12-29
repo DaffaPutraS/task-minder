6f943c5654fbfac6b562ad7a4127e62257052e89
DaffaPutraS <daffap434@gmail.com>
2023-12-29 20:56:41 +0700

    create login page

--- login-page.php
+++ login-page.php
@@ -12,12 +12,31 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
     <!-- Icon Title Link-->
-    <link rel="Icon" href="image/logo-dapskuy.png" type="image/x-icon">
+    <link rel="Icon" href="img/logo.png" type="image/x-icon">
 
     <!-- Font Awesome CDN Link -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 </head>
 <body>
+    <section class="login-container">
+        <div class="book-img">
+            <img src="img/buku.png" alt="Books Image"/>
+        </div>
+        <div class="form">
+            <h2>Sign In</h2>
+            <label for="nama"><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="nama" required>
+
+            <label for="password"><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="password" required>
+
+            <img src="Proses/captcha.php" alt="gambar"><br> <!-- tentukan letak script generate gambar -->
+            <label for="kodecaptcha">Isikan Captcha</label>
+            <input type="text" class="custom-form-control" placeholder="captcha" name="kodecaptcha" value="" maxlength="5" required>
+
+            <button type="submit" class="btn">Login</button>
+        </div>
+    </section>
     
 </body>
 </html>
