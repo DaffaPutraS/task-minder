147cd028c846aa3d29b6f4cf90f8b320e381d127
DaffaPutraS <daffap434@gmail.com>
2023-12-31 03:49:37 +0700

    update kecil

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dashboard | TaskMinder</title>
 
+    <!-- CSS only -->
     <link rel="stylesheet" href="css/style-index.css">
+
+    <!--- Bootstrap Link-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <!-- Icon Title Link-->
+    <link rel="Icon" href="img/logo.png" type="image/x-icon">
+
+    <!-- Font Awesome CDN Link -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 </head>
 <body>
     <h1>asu</h1>
